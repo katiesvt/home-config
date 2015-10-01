428ae285d53a3d0cfb1f11fbb8056cff35cf4cbf
Katelyn Sievert <katelyn.sievert@gmail.com>
2015-10-01 14:44:51 -0700

    add abbreviations to replace functions

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,8 @@ set -g OMF_CONFIG $HOME/.config/omf
 source $OMF_PATH/init.fish
 
 rvm default
+abbr g git
+abbr bi 'bundle install'
+abbr gp 'git push'
+abbr gpf 'git push --force'
+abbr gl 'git pull'
